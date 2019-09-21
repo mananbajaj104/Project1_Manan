dd25635f0ea265c4807b2f704c08c1a6137b9bdd
mbajaj23 <manan.bajaj@gmail.com>
2019-09-21 15:08:43 -0500

    Manan

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Website from './pages/Website.js'
+
+function App() {
+  return (
+  <div>
+
+    <Website />
+    <img src={logo} />  
+  </div>
+  
+  );
+}
+
+export default App;
